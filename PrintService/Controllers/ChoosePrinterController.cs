c91d26984b03c2d3071a9b442cf305dc076e7480
Thinute <tongthanhthao2009@gmail.com>
2024-11-07 20:23:50 +0700

    Profile and Choose printer UI

---
+++ PrintService/Controllers/ChoosePrinterController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PrintService.Controllers
+{
+    public class ChoosePrinterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ PrintService/Controllers/UserInfoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PrintService.Controllers
+{
+    public class UserInfoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
