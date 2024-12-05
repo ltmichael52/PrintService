2fe556468f1fb19da1f3a67b04ed95595bcb6f57
Thinute <tongthanhthao2009@gmail.com>
2024-12-05 21:05:02 +0700

    Printer Setting

---
+++ PrintService/Controllers/PrinterSetting.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PrintService.Controllers
+{
+    public class PrinterSetting : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
