885162713f4e2f26437bf9e4f985e24152946b1f
Thinute <tongthanhthao2009@gmail.com>
2024-12-05 22:42:50 +0700

    Printer Setting and Paper Sheets Purchase

---
+++ PrintService/Controllers/PurchaseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PrintService.Controllers
+{
+    public class PurchaseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
