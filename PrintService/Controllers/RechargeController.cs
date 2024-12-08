64e2a5441c3bef0a99b27596fd0ce13209271f6f
Thinute <tongthanhthao2009@gmail.com>
2024-12-08 19:43:11 +0700

    recharge FE

---
+++ PrintService/Controllers/RechargeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PrintService.Controllers
+{
+    public class RechargeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
