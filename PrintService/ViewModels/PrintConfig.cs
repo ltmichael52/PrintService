924cea36feef33509374bb49956f7d1388995872
lephat <ltmichael52@gmail.com>
2024-12-08 11:52:35 +0700

    feat: [ISSUE-5] Printing process BE

---
+++ PrintService/ViewModels/PrintConfig.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PrintService.ViewModels
+{
+    public class PrintConfig
+    {
+        public int PrinterID { get; set; }
+        public string? PrinterModel { get; set; }
+        public int A3Amount {  get; set; }
+        public int A4Amount { get; set; }
+        public string? CampusName {  get; set; }
+        public string? BuildingName {  get; set; }
+        public string? Room {  get; set; }
+
+        [Required(ErrorMessage = "File is required.")]
+        public IFormFile? File { get; set; }
+
+        [Required(ErrorMessage = "PaperTypeID is required.")]
+        public int PaperTypeID { get; set; }
+
+        [Required(ErrorMessage = "Colored field is required.")]
+        public bool Colored { get; set; }
+
+        [Required(ErrorMessage = "Number of Copies is required.")]
+        [Range(1, int.MaxValue, ErrorMessage = "Number of Copies must be at least 1.")]
+        public int NumberOfCopies { get; set; }
+
+        [Required(ErrorMessage = "IsDoubledSide field is required.")]
+        public bool IsDoubledSide { get; set; }
+
+
+    }
+}
