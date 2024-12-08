11651c0801249adeec1bce108b83d9193e0cb9da
lephat <ltmichael52@gmail.com>
2024-12-08 11:55:16 +0700

    feat: [ISSUE-11] Printer List controller temp

---
+++ PrintService/Controllers/PrinterController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrintService.Models;
+
+namespace PrintService.Controllers
+{
+    public class PrinterController : Controller
+    {
+        private readonly PrintDbContext db;
+        public PrinterController(PrintDbContext context)
+        {
+            db = context;
+        }
+        public IActionResult Index()
+        {
+            List<Printer> printerList = db.Printers.ToList();
+
+            return View();
+        }
+    }
+}
