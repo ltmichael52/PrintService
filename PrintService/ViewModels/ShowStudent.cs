6b97faf3c05ca125e148cf40c6502e85ab6523b4
lephat <ltmichael52@gmail.com>
2024-12-09 11:01:09 +0700

    feat: [ISSUE-12] User Info BE

---
+++ PrintService/ViewModels/ShowStudent.cs
@@ -0,0 +1,13 @@
+﻿namespace PrintService.ViewModels
+{
+    public class ShowStudent
+    {
+        public string? studentId { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public decimal? Balance { get; set; }
+        public int A3Amount { get; set; }
+        public int A4Amount { get; set; }
+
+    }
+}
